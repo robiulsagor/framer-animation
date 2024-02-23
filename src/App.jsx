6f17f6e30426bf0e0b4c626e2049b21df37f46d0
Robiul Islam Sagar <robiul100.me@gmail.com>
2024-02-23 13:17:08 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Animate from "./components/Animate";
+
+export default function App() {
+  return <Animate/>
+}
---
+++ src/components/Animate.jsx
@@ -0,0 +1,24 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+export default function Animate() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <AnimatePresence>
+      <section className="h-screen bg-purple-700 p-2">
+        <div className="w-[400px] mx-auto ">
+          <motion.button
+            className="flex justify-between items-center bg-white w-full p-2 rounded text-2xl font-anta"
+            onClick={() => setIsOpen(!isOpen)}
+            whileTap={{ scale: 0.96 }}
+            transition={{ duration: 0.1 }}
+          >
+            <span>Menu</span>
+            <span className="border-8 border-transparent border-t-black"></span>
+          </motion.button>
+        </div>
+      </section>
+    </AnimatePresence>
+  );
+}
